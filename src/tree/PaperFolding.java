bfa6652b9fa5d8624bb2567980d18447907869dc
chensongxian <1164077611@qq.com>
2018-01-28 16:25:17 +0800

    回文链表

---
+++ src/tree/PaperFolding.java
@@ -0,0 +1,29 @@
+package tree;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: TODO
+ * @Author: csx
+ * @Date: 2018-01-27
+ */
+public class PaperFolding {
+    public static void printAllFolds(int N) {
+        printProcess(1, N, true);
+    }
+
+    public static void printProcess(int i, int N, boolean down) {
+        if (i > N) {
+            return;
+        }
+        printProcess(i + 1, N, true);
+        System.out.println(down ? "down " : "up ");
+        printProcess(i + 1, N, false);
+    }
+
+    public static void main(String[] args) {
+        int N = 4;
+        printAllFolds(N);
+
+    }
+}
