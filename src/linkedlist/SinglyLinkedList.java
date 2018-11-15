be92833c5af5b3519ed3b3a5eb1fe69efa452ab6
chensongxian <1164077611@qq.com>
2018-11-15 17:03:28 +0800

    '链表'

--- src/linkedlist/SinglyLinkedList.java
+++ src/linkedlist/SinglyLinkedList.java
@@ -86,7 +86,74 @@ public class SinglyLinkedList {
         p.next = newNode;
     }
 
-    public void insertBefore(Node p, Node newNode) {
+    public void insertBefore(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertBefore(p, newNode);
+    }
 
+    public void insertBefore(Node p, Node newNode) {
+        if (p == null) {
+            return;
+        }
+
+        if (p == head) {
+            insertToHead(newNode);
+            return;
+        }
+
+        Node q = head;
+        while (q != null && q.next != p ) {
+            q = q.next;
+        }
+
+        if (q == null) {
+            return;
+        }
+
+        newNode.next = p;
+        q.next = newNode;
+    }
+
+    public void deleteByNode(Node p) {
+        if (p == null || head == null) {
+            return;
+        }
+
+        if (p == head) {
+            head = head.next;
+            return;
+        }
+
+        Node q = head;
+        while (q != null && q.next != p) {
+            q = q.next;
+        }
+
+        if (q == null) {
+            return;
+        }
+        q.next = q.next.next;
+    }
+
+    public void deleteByValue(int value) {
+        if (head == null) {
+            return;
+        }
+
+        Node p = head;
+        Node q = null;
+        while (p != null && p.data != value) {
+            q = p;
+            p = p.next;
+        }
+
+        if (p == null) {
+            return;
+        }
+        if (q == null) {
+            head = head.next;
+        } else {
+            q.next = p.next.next;
+        }
     }
 }
